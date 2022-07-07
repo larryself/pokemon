5ac9f07d899fba0c6777ae3f3acbcf17ca9320f7
Yan Musikhin <yanmusikhin@gmail.com>
2022-07-07 16:45:50 +0400

    feat: add skeleton

---
+++ src/components/skeleton/skeletonCard/skeletonCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Info, Stat, StatsInner, FeaturesInner, Shimmer,ShimmerWrapper } from './style';
+import { SkeletonElement } from '../skeletonElement/skeletonElement';
+
+export const SkeletonCard = () => {
+  return (
+    <Container>
+      <Info>
+        <SkeletonElement type={'title'}/>
+        <StatsInner>
+          <Stat>
+            <SkeletonElement type={'stats'}/>
+            <SkeletonElement type={'stats-name'}/>
+          </Stat>
+          <Stat>
+            <SkeletonElement type={'stats'}/>
+            <SkeletonElement type={'stats-name'}/>
+          </Stat>
+        </StatsInner>
+        <FeaturesInner>
+          <SkeletonElement type={'feature'}/>
+        </FeaturesInner>
+      </Info>
+      <SkeletonElement type={'image'}/>
+      <ShimmerWrapper>
+        <Shimmer/>
+      </ShimmerWrapper>
+    </Container>
+  );
+};
