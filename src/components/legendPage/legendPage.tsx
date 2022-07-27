5edb11fe4a2478e3355121b90971fc7c26e57ca2
Yan Musikhin <yanmusikhin@gmail.com>
2022-07-27 20:35:54 +0400

    feat: add legendPage

--- src/components/legendPage/legendPage.tsx
+++ src/components/legendPage/legendPage.tsx
@@ -1,7 +1,48 @@
-import { Footer, Header, Wrapper } from 'components';
-import { Inner, Container } from './style';
+import { Footer, Header, Wrapper, Carousel, Card } from 'components';
+import { Container, Inner, Line, Title } from './style';
+import { useGetPokemonQuery, usePokemonSelector } from 'store';
+import { useEffect, useState } from 'react';
+import { PokemonProps } from '../cartItem/cartItem';
 
 export const LegendPage = () => {
+  const {pokemons} = usePokemonSelector();
+  const [legend, setLegend] = useState<PokemonProps | null>(null);
+  const [strong, setStrong] = useState<PokemonProps | null>(null);
+  const [waek, setWeak] = useState<PokemonProps | null>(null);
+  const [legendPokemon, setLegendPokemon] = useState<PokemonProps[]>([]);
+  const [strongPokemon, setStrongPokemon] = useState<PokemonProps[]>([]);
+  const [weakPokemon, setWeakPokemon] = useState<PokemonProps[]>([]);
+  const result = useGetPokemonQuery();
+
+  const getLegendPokemon = (pokemons: PokemonProps[]) => {
+    return pokemons.filter((pokemon) => {
+      const stat = pokemon.stats.reduce((acc, item) => item.base_stat + acc, 0);
+      return stat > 550;
+    });
+  };
+
+  const getStrongerPokemon = (pokemons: PokemonProps[]) => {
+    return pokemons.filter((pokemon) => {
+      return pokemon.stats[1].base_stat >= 100 && pokemon.stats[3].base_stat >= 100;
+    });
+  };
+
+  const getWeaketPokemon = (pokemons: PokemonProps[]) => {
+    return pokemons.filter((pokemon) => {
+      return pokemon.stats[1].base_stat <= 30 && pokemon.stats[3].base_stat <= 30;
+    });
+  };
+
+  useEffect(() => {
+    if (result.status === 'fulfilled' || pokemons.length > 0) {
+      const legends = getLegendPokemon(pokemons);
+      const weakers = getWeaketPokemon(pokemons);
+      const strongers = getStrongerPokemon(pokemons);
+      setWeakPokemon(weakers);
+      setStrongPokemon(strongers);
+      setLegendPokemon(legends);
+    }
+  }, [result.status, pokemons.length]);
   return (
     <>
       <Header/>
@@ -9,7 +50,18 @@ export const LegendPage = () => {
         <main>
           <Wrapper>
             <Container>
-              <h1>LegendPage</h1>
+              <Title>Legendaries</Title>
+              <Line/>
+              <Card pokemon={legend}/>
+              <Carousel pokemons={legendPokemon} selectPokemon={setLegend}/>
+              <Title>Stronger</Title>
+              <Line/>
+              <Card pokemon={strong}/>
+              <Carousel pokemons={strongPokemon} selectPokemon={setStrong}/>
+              <Title>Weaker</Title>
+              <Line/>
+              <Card pokemon={waek}/>
+              <Carousel pokemons={weakPokemon} selectPokemon={setWeak}/>
             </Container>
           </Wrapper>
         </main>
--- src/components/legendPage/style.ts
+++ src/components/legendPage/style.ts
@@ -1,15 +1,37 @@
 import styled from 'styled-components';
+import { BREAKPOINTS } from '../../constants/breakpoints';
 
 export const Inner = styled.div`
   background: ${({theme}) => theme.bgContentPokedex};
   color: ${({theme}) => theme.text};
   display: flex;
   flex-direction: column;
-  height: 100%;
+  flex-grow: 1;
 `;
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   padding-top: 39px;
   padding-bottom: 50px;
+`;
+
+export const Title = styled.h2`
+  font-family: 'Karla';
+  font-weight: 400;
+  font-size: 7.2rem;
+  line-height: 1.166;
+  letter-spacing: 4px;
+  color: ${({theme}) => theme.text};
+  @media (max-width: ${BREAKPOINTS.sm}) {
+    font-size: 4.4rem;
+    line-height: 1.159;
+  }
+`;
+
+export const Line = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: ${({theme}) => theme.text};
+  margin-bottom: 45px;
 `;
